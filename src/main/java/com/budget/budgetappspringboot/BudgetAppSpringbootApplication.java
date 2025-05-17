1dd3bff6491e62f64f2dd2a62efb3ae5a57c84b0
user1 <user1@localhost.localdomain>
2025-05-17 09:16:18 -0500

    Initial commit on project setup. Main branch. Working branch to follow.

---
+++ src/main/java/com/budget/budgetappspringboot/BudgetAppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.budget.budgetappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAppSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetAppSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/budget/budgetappspringboot/BudgetAppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.budgetappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAppSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
