d054cfc8527cb1615aa0ea3530de59f95945c621
devichs <steff.devich@gmail.com>
2025-05-19 06:05:40 -0500

    Bringing main in sync with working

--- src/main/java/com/budget/budgetappspringboot/repository/AccountRepository.java
+++ src/main/java/com/budget/budgetappspringboot/repository/AccountRepository.java
@@ -7,11 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-<<<<<<< HEAD
-public interface AccountRepository {
-=======
 public interface AccountRepository extends JpaRepository<Account, Long> {
->>>>>>> working
     Optional<Account> findByName(String name);
     Optional<Account> findByNameIgnoreCase(String name);
 }
